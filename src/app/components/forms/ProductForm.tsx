55bc2a5226956d2c9de575e9b2b663ac5ef59cc1
MaxLiebsch <liebsch@dipmaxexport.com>
2024-02-12 07:48:17 +0100

    fix: add clear form button

--- src/app/components/forms/ProductForm.tsx
+++ src/app/components/forms/ProductForm.tsx
@@ -167,6 +167,15 @@ const ProductForm = ({
   //   onSuccess
   // })
 
+  const handleClearForm = () => {
+    setCompleting(false);
+    sessionStorage.removeItem("current");
+    sessionStorage.removeItem("description");
+    sessionStorage.removeItem("productid");
+    sessionStorage.removeItem("details");
+    reset();
+  };
+
   const handleOnSuccess = async (response: Models.Document) => {
     setCompleting(true);
     const productId = sessionStorage.getItem("productid");
@@ -226,13 +235,8 @@ const ProductForm = ({
       id,
     })
       .then((response) => {
-        setCompleting(false);
-        sessionStorage.removeItem("current");
-        sessionStorage.removeItem("description");
-        sessionStorage.removeItem("productid");
-        sessionStorage.removeItem("details");
         onProductPageCreated({ slug: response.slug, success: true });
-        reset();
+        handleClearForm();
       })
       .catch((error) => {
         enqueueSnackbar({
@@ -361,39 +365,52 @@ const ProductForm = ({
           className="flex justify-between relative gap-2 flex-col"
         >
           <div className="flex flex-row gap-3 items-center ml-auto absolute top-6 right-0 mt-2">
-            {isSubmitting || completing && <MyTimer duration={90} />}
+            {isSubmitting || (completing && <MyTimer duration={90} />)}
             <Button
               disabled={
-                descriptionMutation.isPending || detailsMutation.isPending || completing
+                descriptionMutation.isPending ||
+                detailsMutation.isPending ||
+                completing
               }
               variant="outlined"
               type="submit"
             >
-              {isSubmitting || completing && (
-                <svg
-                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-primary"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx={12}
-                    cy={12}
-                    r={10}
-                    stroke="currentColor"
-                    strokeWidth={4}
-                  />
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  />
-                </svg>
-              )}
+              {isSubmitting ||
+                (completing && (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-primary"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx={12}
+                      cy={12}
+                      r={10}
+                      stroke="currentColor"
+                      strokeWidth={4}
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>
+                ))}
               {sessionStorage.getItem("productid")
                 ? "Complete Page"
                 : "Create Page"}
+            </Button>
+            <Button
+              variant="outlined"
+              color="warning"
+              onClick={() => {
+                handleClearForm();
+                onPageContentChange(undefined);
+              }}
+            >
+              Clear form
             </Button>
           </div>
           <h1 className="text-3xl mt-8">Product Information</h1>
