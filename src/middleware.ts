9fb8e53a86e1b5dde65166ddba6b29c400f3b62c
MaxLiebsch <liebsch@dipmaxexport.com>
2024-02-12 10:41:40 +0100

    fix: console.log lookup

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,34 +2,43 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const url = request.url;
-  if (url.includes("/product")) {
-    let ip;
-    const xForwardedFor = request.headers.get("x-forwarded-for");
-    const xRealIp = request.headers.get("x-real-ip");
-    if (xForwardedFor) {
-      ip = xForwardedFor.split(",")[0];
-    } else if (xRealIp) {
-      ip = xRealIp;
-    } else {
-      ip = "unknown";
-    }
-    console.log("ip:", ip);
 
-    const lookup = await (
-      await fetch(process.env.NEXT_PUBLIC_FE_BASEURL! + "/api/ipinfo/" + ip)
-      ).json();
-    
-    console.log('lookup:', lookup)
-
-    if (
-      lookup.error === "ip not found" ||
-      lookup.city_name !== "Berlin" ||
-      lookup.iso_code !== "BD"
-    ) {
-      return NextResponse.redirect(process.env.NEXT_PUBLIC_FE_BASEURL! + "/");
-    }
+  let ip;
+  const xForwardedFor = request.headers.get("x-forwarded-for");
+  const xRealIp = request.headers.get("x-real-ip");
+  if (xForwardedFor) {
+    ip = xForwardedFor.split(",")[0];
+  } else if (xRealIp) {
+    ip = xRealIp;
   } else {
-    const response = NextResponse.next();
-    return response;
+    ip = "unknown";
   }
+  console.log("ip:", ip);
+
+  const lookup = await (
+    await fetch(process.env.NEXT_PUBLIC_FE_BASEURL! + "/api/ipinfo/" + ip)
+  ).json();
+
+  console.log(
+    "lookup:",
+    lookup.city_name,
+    lookup.iso_code,
+    "conditon",
+    lookup.city_name !== "Berlin" || lookup.iso_code !== "BD"
+  );
+
+  if (lookup.city_name !== "Berlin" || lookup.iso_code !== "BD") {
+    return NextResponse.redirect(process.env.NEXT_PUBLIC_FE_BASEURL! + "/");
+  }
+
+  if (lookup.error === "ip not found") {
+    return NextResponse.redirect(process.env.NEXT_PUBLIC_FE_BASEURL! + "/");
+  }
+
+  const response = NextResponse.next();
+  return response;
 }
+
+export const config = {
+  matcher: "/product/:path*",
+};
