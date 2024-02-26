fb37f1d927926c29280f57bf393c65fcbd4d48ac
MaxLiebsch <liebsch@dipmaxexport.com>
2024-02-26 16:35:22 +0100

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,30 @@
+
+
+
 import { redirect } from "next/navigation";
+import AuthProvider from "../lib/providers/AuthProvider";
+
+
+export interface PageContent {
+  p: string;
+  a: string;
+  n: string;
+  f: string; //features highlight
+  pzn: string;
+  m: string; //details
+  link: string;
+  ps: string; //description
+  ls: any[];
+  ean: string;
+  img: string[];
+}
 
 export default function Page({ params }: { params: { slug: string } }) {
-  redirect("/admin/search");
+  redirect('/admin/search')
+  return (
+    <AuthProvider>
+      <div> 
+      </div>
+    </AuthProvider>
+  );
 }
