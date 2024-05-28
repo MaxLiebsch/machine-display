9253d8716a356662614c0dfcc35888594478e6ac
MaxLiebsch <liebsch@dipmaxexport.com>
2024-05-28 08:06:26 +0200

    feat: version number

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -76,7 +76,7 @@ const navigation = [
           </div>
           <div className="mt-2 sm:mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500">
-              &copy; 2024 DipMax Export GmbH All rights reserved.
+              &copy; 2024 DipMax Export GmbH All rights reserved. - v0.1.1
             </p>
           </div>
         </div>
